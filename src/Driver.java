c3e77c55a80498922afdaf113d1008f54093bfb4
Iker Goni <igoni@cpp.edu>
2022-04-27 17:14:12 -0700

    instantiated graph in Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,17 @@
 public class Driver {
     public static void main(String[] args){
-
+        Graph graph = new Graph(9);
+        graph.addEdge('A','B');
+        graph.addEdge('A','E');
+        graph.addEdge('A','D');
+        graph.addEdge('B','E');
+        graph.addEdge('E','F');
+        graph.addEdge('E','H');
+        graph.addEdge('D','G');
+        graph.addEdge('G','H');
+        graph.addEdge('H','I');
+        graph.addEdge('I','F');
+        graph.addEdge('F','C');
+        graph.addEdge('C','B');
     }
 }
