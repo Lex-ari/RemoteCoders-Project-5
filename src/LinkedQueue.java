ccf21414755b1e4884b884138ba0222b772299f0
Alex Mariano <amarianocello@gmail.com>
2022-04-29 20:02:13 -0700

    Added private Node class to LinkedQueue and default constructor - Alex

--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -1,2 +1,58 @@
-public class LinkedQueue {
+public class LinkedQueue<T> implements QueueInterface<T> {
+    private Node firstNode;
+    private Node lastNode;
+    public LinkedQueue(){
+        firstNode = null;
+        lastNode = null;
+    }
+
+    private class Node{
+        private T data;
+        private Node next;
+
+
+
+
+        public T getData(){
+            return data;
+        }
+
+        public void setData(T newData){
+            data = newData;
+        }
+
+        public Node getNextNode(){
+            return next;
+        }
+
+        public void setNextNode(Node newNode){
+            next = newNode;
+        }
+    }
+
+
+    @Override
+    public void enqueue(T newEntry) {
+
+    }
+
+    @Override
+    public T dequeue() {
+        return null;
+    }
+
+    @Override
+    public T getFront() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
