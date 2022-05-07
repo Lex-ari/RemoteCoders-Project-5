27ad6afc068b651ec4fd74ccd7e8824eaae7fbf9
Koki <kyamaguchi@cpp.edu>
2022-05-07 13:43:00 -0700

    implemented code for the stack test

--- src/BasicGraphInterface.java
+++ src/BasicGraphInterface.java
@@ -1,15 +1,45 @@
 public interface BasicGraphInterface<T> {
+
+    /**
+     * @param vertexLabel
+     * @return
+     */
     public boolean addVertex(T vertexLabel);
 
+    /**
+     * @param begin
+     * @param end
+     * @param edgeWeight
+     * @return
+     */
     public boolean addEdge(T begin, T end, double edgeWeight);
 
+    /**
+     * @param begin
+     * @param end
+     * @return
+     */
     public boolean addEdge(T begin, T end);
 
+    /**
+     * @param begin
+     * @param end
+     * @return
+     */
     public boolean hadEdge(T begin, T end);
 
+    /**
+     * @return
+     */
     public boolean isEmpty();
 
+    /**
+     * @return
+     */
     public int getNumberOfVertices();
 
+    /**
+     *
+     */
     public void clear();
 }
--- src/JunitTesting.java
+++ src/JunitTesting.java
@@ -1,2 +1,93 @@
-public class JunitTesting {
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JunitTesting {
+    /*
+     *  initialized
+     */
+    QueueInterface<Character> linkedQueue = new LinkedQueue();
+    StackInterface<Character> linkedStack = new LinkedStack();
+
+    @Before
+    public void setUp(){
+        testClear();
+    }
+
+    @Test
+    public void testClear(){
+        linkedStack.clear();
+        assertEquals(true, linkedStack.isEmpty());
+        linkedStack.push('a');
+        linkedStack.clear();
+        assertEquals(true, linkedStack.isEmpty());
+    }
+    @Test
+    public void testPush(){
+        try {
+            linkedStack.push('a');
+        } catch (Exception e){
+            fail("Failure: push() threw an exception: " + e);
+        }
+        try {
+            linkedStack.push('b');
+        } catch (Exception e){
+            fail("Failure: push() threw an exception: " + e);
+        }
+        try {
+            linkedStack.push(null);
+        } catch (Exception e){
+            fail("Failure: push() threw an exception: " + e);
+        }
+    }
+    @Test
+    public void testPop(){
+        try {
+            linkedStack.pop();
+            fail("Failure: Successfully pop into an empty bag.");
+        } catch (EmptyStackException e){
+            assertEquals(EmptyStackException.class, e.getClass());
+        }
+        linkedStack.push('a');
+        linkedStack.push('b');
+        assertEquals('b', linkedStack.pop());
+        assertEquals('a', linkedStack.pop());
+    }
+    @Test
+    public void testPeek(){
+        try {
+            linkedStack.peek();
+            fail("Failure: Successfully peeked into an empty bag.");
+        } catch (EmptyStackException e){
+            assertEquals(EmptyStackException.class, e.getClass());
+        }
+        linkedStack.push('a');
+        assertEquals('a', linkedStack.peek());
+    }
+    @Test
+    public void testIsEmpty(){
+        assertEquals(true, linkedStack.isEmpty());
+        linkedStack.push('a');
+        assertEquals(false, linkedStack.isEmpty());
+    }
+
+
+
+
+
+
+
+
+
+
+    @Test
+    void breadthFirstTraversal() {
+    }
+
+    @Test
+    void depthFirstTraversal() {
+    }
 }
