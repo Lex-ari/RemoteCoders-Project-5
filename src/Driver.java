86102c2ef7536896bab44ef905f719efa1f64894
Alex Mariano <amarianocello@gmail.com>
2022-05-09 03:36:52 -0700

    Properly Created Graph in Driver.java - Alex

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,31 @@
 public class Driver {
     public static void main(String[] args){
+
+        // Let A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7, I = 8
+
         Graph graph = new Graph(9);
-        graph.addEdge('A','B');
-        graph.addEdge('A','E');
-        graph.addEdge('A','D');
-        graph.addEdge('B','E');
-        graph.addEdge('E','F');
-        graph.addEdge('E','H');
-        graph.addEdge('D','G');
-        graph.addEdge('G','H');
-        graph.addEdge('H','I');
-        graph.addEdge('I','F');
-        graph.addEdge('F','C');
-        graph.addEdge('C','B');
+        graph.setLabel(0,'A');
+        graph.setLabel(1,'B');
+        graph.setLabel(2,'C');
+        graph.setLabel(3,'D');
+        graph.setLabel(4,'E');
+        graph.setLabel(5,'F');
+        graph.setLabel(6,'G');
+        graph.setLabel(7,'H');
+        graph.setLabel(8,'I');
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 3);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 1);
+        graph.addEdge(3, 6);
+        graph.addEdge(4, 5);
+        graph.addEdge(4, 7);
+        graph.addEdge(5, 7);
+        graph.addEdge(5, 2);
+        graph.addEdge(7, 8);
+        graph.addEdge(8, 5);
+
     }
 }
